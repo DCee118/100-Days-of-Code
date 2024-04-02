1ad78b4533b6d72855856a2afb2e3d03da7f29ce
DCee118 <dominicchambers1@hotmail.co.uk>
2024-04-02 20:56:56 +0100

    day 4 complete

---
+++ day-4/rock-paper-scissors.py
@@ -0,0 +1,58 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+player_choice = int(input('What do you choose? \n0 for Rocks \n1 for Paper \n2 for Scissors \n'))
+if player_choice == 0:
+    print(rock)
+elif player_choice == 1:
+    print(paper)
+elif player_choice == 2:
+    print(scissors)
+    
+computer_choice = random.randint(0,2)
+print('Computer chose:')
+if computer_choice == 0:
+    print(rock)
+elif computer_choice == 1:
+    print(paper)
+elif computer_choice == 2:
+    print(scissors) 
+
+if player_choice >= 3 or player_choice < 0:
+    print('Invalid chose! You lose!')
+elif player_choice == 1 and computer_choice == 0:
+    print('You win!')
+elif player_choice == 2 and computer_choice == 1:
+    print('You win!')
+elif player_choice == computer_choice:
+    print('Draw')
+elif player_choice == 0 and computer_choice == 2:
+    print('You win!')
+else:
+    print('You lose!')
